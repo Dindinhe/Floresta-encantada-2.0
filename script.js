279d3fd5c3840d4d89d4f0232f4450bee9a8ad80
Dindinhe <0000108248815xsp@al.educacao.sp.gov.br>
2024-10-08 11:23:59 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+sarah chalegra 3C- 08/10/2024
+
+const avanca = document.querySelectorAll('.btn-proximo');
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
